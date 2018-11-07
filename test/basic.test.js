b5627f47cbf124c4e78026a7017d5fb4e9b07359
Clark Du <clark.duxin@gmail.com>
2018-11-07 17:00:43 +0000

    test: fix basic test

--- test/basic.test.js
+++ test/basic.test.js
@@ -4,19 +4,7 @@ import webpack from 'webpack';
 
 import basicConfig from './fixtures/basic/webpack.config';
 
-const logUpdateMethods = ['clear'];
-
-const mockLogUpdate = () => {
-  const m = jest.fn();
-  logUpdateMethods.forEach((k) => {
-    m[k] = jest.fn();
-  });
-  return m;
-};
-
 describe('webpackbar', () => {
-  const logUpdate = mockLogUpdate();
-
   test('compile', async () => {
     const done = jest.fn();
 
@@ -26,8 +14,9 @@ describe('webpackbar', () => {
         minimal: false,
         profile: true,
         color: '#202020',
-        logUpdate,
-        done,
+        reporter: {
+          done,
+        },
       })
     );
 
@@ -38,14 +27,4 @@ describe('webpackbar', () => {
     expect(stats.hasWarnings()).toBe(false);
     expect(done).toHaveBeenCalledTimes(1);
   });
-
-  test('logUpdate', () => {
-    expect(logUpdate).toBeCalled();
-  });
-
-  for (const method of logUpdateMethods) {
-    test(`logUpdate.${method}`, () => {
-      expect(logUpdate[method]).toBeCalled();
-    });
-  }
 });

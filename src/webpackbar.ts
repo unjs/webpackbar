7498b9574a04c884d2c26eb3f00013c4d3875377
Pooya Parsa <pyapar@gmail.com>
2020-11-02 23:03:59 +0100

    refactor: improve reporter resolving

--- src/webpackbar.ts
+++ src/webpackbar.ts
@@ -1,3 +1,217 @@
-import plugin from './plugin'
+import { ProgressPlugin } from 'webpack'
+import env from 'std-env'
+import prettyTime from 'pretty-time'
 
-export default plugin
+import { startCase, shortenPath, objectValues } from './utils'
+import { WebpackBarOptions, ReporterOpts, Reporter } from './types'
+
+import * as reporters from './reporters'
+import { parseRequest, hook } from './utils/webpack'
+
+// Default plugin options
+const DEFAULTS = {
+  name: 'webpack',
+  color: 'green',
+  reporters: env.minimalCLI ? ['basic'] : ['fancy'],
+  reporter: null
+}
+
+// Default state object
+const DEFAULT_STATE = {
+  start: null,
+  progress: -1,
+  done: false,
+  message: '',
+  details: [],
+  request: null,
+  hasErrors: false
+}
+
+// Mapping from name => State
+const globalStates = {}
+
+export default class WebpackBarPlugin extends ProgressPlugin {
+  private options: any
+  private reporters: Reporter[]
+
+  constructor(options: WebpackBarOptions) {
+    super({ activeModules: true })
+
+    this.options = Object.assign({}, DEFAULTS, options)
+
+    // Assign a better handler to base ProgressPlugin
+    this.handler = (percent, message, ...details) => {
+      this.updateProgress(percent, message, details)
+    }
+
+    // Reporters
+    const _reporters: ReporterOpts[] = Array
+      .from(this.options.reporters || [])
+      .concat(this.options.reporter)
+      .filter(Boolean)
+      .map(reporter => {
+        if (Array.isArray(reporter)) {
+          return { reporter: reporter[0], options: reporter[1] }
+        }
+        if (typeof reporter === 'string') {
+          return { reporter }
+        }
+        return reporter as ReporterOpts
+      })
+
+    // Resolve reporters
+    this.reporters = _reporters.map(({ reporter, options = {} }) => {
+      if (typeof reporter === 'string') {
+        if (this.options[reporter] === false) {
+          return
+        }
+        options = { ...this.options[reporter], ...options }
+        reporter = (reporters[reporter] || require(reporter)) as Reporter
+      }
+
+      if (typeof reporter.constructor === 'function') {
+        // @ts-ignore
+        reporter = new reporter(options)
+      } else if (typeof reporter === 'function') {
+        // @ts-ignore
+        reporter = reporter(options)
+      }
+
+      return reporter as Reporter
+    }).filter(Boolean)
+  }
+
+  callReporters(fn, payload = {}) {
+    for (const reporter of this.reporters) {
+      if (typeof reporter[fn] === 'function') {
+        try {
+          reporter[fn](this, payload)
+        } catch (e) {
+          process.stdout.write(e.stack + '\n')
+        }
+      }
+    }
+  }
+
+  get hasRunning() {
+    return objectValues(this.states).some(state => !state.done)
+  }
+
+  get hasErrors() {
+    return objectValues(this.states).some(state => state.hasErrors)
+  }
+
+  get statesArray() {
+    return objectValues(this.states).sort((s1, s2) =>
+      s1.name.localeCompare(s2.name)
+    )
+  }
+
+  get states() {
+    return globalStates
+  }
+
+  get state() {
+    return globalStates[this.options.name]
+  }
+
+  _ensureState() {
+    // Keep our state in shared object
+    if (!this.states[this.options.name]) {
+      this.states[this.options.name] = {
+        ...DEFAULT_STATE,
+        color: this.options.color,
+        name: startCase(this.options.name)
+      }
+    }
+  }
+
+  apply(compiler) {
+    // Prevent adding multi instances to the same compiler
+    if (compiler.webpackbar) {
+      return
+    }
+    compiler.webpackbar = this
+
+    // Apply base hooks
+    super.apply(compiler)
+
+    // Register our state after all plugins initialized
+    hook(compiler, 'afterPlugins', () => {
+      this._ensureState()
+    })
+
+    // Hook into the compiler before a new compilation is created.
+    hook(compiler, 'compile', () => {
+      this._ensureState()
+
+      Object.assign(this.state, {
+        ...DEFAULT_STATE,
+        start: process.hrtime()
+      })
+
+      this.callReporters('start')
+    })
+
+    // Watch compilation has been invalidated.
+    hook(compiler, 'invalid', (fileName, changeTime) => {
+      this._ensureState()
+
+      this.callReporters('change', {
+        path: fileName,
+        shortPath: shortenPath(fileName),
+        time: changeTime
+      })
+    })
+
+    // Compilation has completed
+    hook(compiler, 'done', (stats) => {
+      this._ensureState()
+
+      // Prevent calling done twice
+      if (this.state.done) {
+        return
+      }
+
+      const hasErrors = stats.hasErrors()
+      const status = hasErrors ? 'with some errors' : 'successfully'
+
+      const time = this.state.start
+        ? ' in ' + prettyTime(process.hrtime(this.state.start), 2)
+        : ''
+
+      Object.assign(this.state, {
+        ...DEFAULT_STATE,
+        progress: 100,
+        done: true,
+        message: `Compiled ${status}${time}`,
+        hasErrors
+      })
+
+      this.callReporters('progress')
+
+      this.callReporters('done', { stats })
+
+      if (!this.hasRunning) {
+        this.callReporters('beforeAllDone')
+        this.callReporters('allDone')
+        this.callReporters('afterAllDone')
+      }
+    })
+  }
+
+  updateProgress(percent = 0, message = '', details = []) {
+    const progress = Math.floor(percent * 100)
+
+    const activeModule = details.pop()
+
+    Object.assign(this.state, {
+      progress,
+      message: message || '',
+      details,
+      request: parseRequest(activeModule)
+    })
+
+    this.callReporters('progress')
+  }
+}

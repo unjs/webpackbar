43cf87ec30bb9074fd55f38a7213b21143bc9524
Pooya Parsa <pyapar@gmail.com>
2021-11-05 14:41:35 +0100

    refactor: simplify changes from #90 and disable due to display issues

--- src/utils/log-update.ts
+++ src/utils/log-update.ts
@@ -36,23 +36,17 @@ export default class LogUpdate {
 
     this.write(data)
 
-    this.prevLineCount = data.split('\n').length
-    this.lineCountPlus(this.extraLines)
+    const _lines = data.split('\n')
+    this.prevLineCount = _lines.length
+
+    // Count wrapped line too
+    // https://github.com/unjs/webpackbar/pull/90
+    // TODO: Count length with regards of control chars
+    // this.prevLineCount += _lines.reduce((s, l) => s + Math.floor(l.length / this.columns), 0)
   }
 
   get columns () {
     return (process.stderr.columns || 80) - 2
-  }
-
-  // calulate the prev line count Math.floor(string width / columns)
-  // here has anohter problem could be ignore it:
-  // if the terminal rows < write(data), the erase line api only clear the viewport lines
-  // it's result that the write(data) is not inline, write(data) is scrollable
-  lineCountPlus (lines) {
-    lines.forEach((line) => {
-      // if the line lenth more than the columns line count + 1
-      this.prevLineCount += Math.floor(line.length / this.columns)
-    })
   }
 
   write (data) {
